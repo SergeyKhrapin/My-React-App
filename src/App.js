ed322fcd7f7849312d57386748877d9461bf24b6
Serhii Khrapin <Serhii_Khrapin@epam.com>
2020-01-24 12:39:36 +0200

    Work with rendering

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{props.time}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +22,7 @@ function App(props) {
         >
           Learn React
         </a>
-        {ListItems(props)}
+        {ListItems(props)} // @TODO: use like a component rather than just a function
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-let arr = [1, 2, 3];
+const arr = [1, 2, 3];
 
-ReactDOM.render(<App items={arr}/>, document.getElementById('root'));
+const getTime = ()=> {
+    const date = new Date();
+
+    const time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+
+    return time.map((el)=> {
+        return el > 9 ? el : `0${el}`;
+    }).join(':');
+}
+
+
+const containerDOM = document.getElementById('root');
+const callback = ()=> {
+    console.log('React component has been rendered :)');
+}
+
+setInterval(()=> {
+    const componentReact = <App items={arr} time={getTime()}/>;
+    return ReactDOM.render(componentReact, containerDOM, callback);
+}, 1000)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
