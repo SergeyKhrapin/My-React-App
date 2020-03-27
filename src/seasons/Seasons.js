779d128e39ba8ca2b17740dfaf332defa7d1a265
Serhii Khrapin <Serhii_Khrapin@epam.com>
2020-03-27 18:46:13 +0200

    Add error handler for Seasons component

--- src/seasons/Seasons.js
+++ src/seasons/Seasons.js
@@ -5,34 +5,54 @@ import snowflake from '../snowflake.png';
 class Seasons extends React.Component {
   constructor() {
     super();
-    this.state = {isWinter: null};
+    this.state = {
+      isWinter: true,
+      errorMessage: ''
+    };
     this.getPosition();
   }
 
   getPosition() {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const currentMonth = new Date().getMonth();
-      const isFromMarchToOctober = currentMonth >= 2 && currentMonth <= 9;
-      const latitude = pos.coords.latitude;
-      const isNorthernHemisphere = latitude > 0 && latitude < 90;
-      const isWinterNow = isNorthernHemisphere && !isFromMarchToOctober || !isNorthernHemisphere && isFromMarchToOctober;
-      this.setState({isWinter: isWinterNow});
-    })
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const currentMonth = new Date().getMonth();
+        const isFromMarchToOctober = currentMonth >= 2 && currentMonth <= 9;
+        const latitude = pos.coords.latitude;
+        const isNorthernHemisphere = latitude > 0 && latitude < 90;
+        const isWinterNow = isNorthernHemisphere && !isFromMarchToOctober || !isNorthernHemisphere && isFromMarchToOctober;
+        this.setState({isWinter: isWinterNow});
+      },
+      err => {
+        this.setState({errorMessage: err.message});
+      }
+    )
   }
 
   styles = {
     image: {
       width: '150px',
     },
+    errorMessage: {
+      color: 'red'
+    }
   }
 
   render() {
-    return (
+    const successHTML = (
       <div>
         <img src={this.state.isWinter ? snowflake : sun} style={this.styles.image}/>
         <h5>Probably it's a {this.state.isWinter ? 'winter' : 'summer'} now :)</h5>
       </div>
     );
+
+    const errorHTML = (
+      <h5 style={this.styles.errorMessage}>
+        Hoops :(<br></br>
+        {this.state.errorMessage}
+      </h5>
+    );
+
+    return !this.state.errorMessage ? successHTML : errorHTML;
   }
 }
 
