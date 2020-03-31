98b1cda943761c8d6cd8352d53bc81bdecffda63
Serhii Khrapin <Serhii_Khrapin@epam.com>
2020-03-31 18:51:36 +0300

    My custom hook

--- src/utilities/Border.js
+++ src/utilities/Border.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Border = props => {
+    // My custom hook (should begin with 'use' prefix)
+    const useInputHandlerAndValue = () => {
+        const [color, setColor] = useState('');
+        return {
+            onChange: event => setColor(event.target.value),
+            value: color
+        };
+    };
+
+    const inputHandlerAndValue = useInputHandlerAndValue();
+
     const styles = {
         padding: '10px',
-        border: `4px solid ${props.children.props.borderColor}`
+        border: `4px solid ${inputHandlerAndValue.value || props.children.props.borderColor}`
     };
 
     return (
         <div style={styles}>
             {props.children}
+            <input placeholder='Change border color' {...inputHandlerAndValue} />
         </div>
     );
-};
-
-Border.defaultProps = {
-    color: 'aqua'
 };
 
 export default Border;
