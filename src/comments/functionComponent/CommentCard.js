18489d5847642c3ffa7ebe9f00e4d6bf88ee68b0
SergeyKhrapin <khrapins@gmail.com>
2021-02-28 16:05:17 +0200

    Prevent class component update - PureComponent

--- src/comments/functionComponent/CommentCard.js
+++ src/comments/functionComponent/CommentCard.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 
-const CommentCard = props => {
-    console.log('CommentCard');
+// PureComponent - if props or state are not changed, component is not updated
+class CommentCard extends PureComponent {
+    constructor() {
+        super();
+    }
 
-    return <li>{props.comment.message} - {Date.now()}</li>;
+    // shouldComponentUpdate() {
+    //     return false; // Prevent component update
+    // }
+
+    render() {
+        console.log('CommentCard');
+        return <li>{this.props.comment.message} - {Date.now()}</li>;
+    };
 }
 
 export default CommentCard;
